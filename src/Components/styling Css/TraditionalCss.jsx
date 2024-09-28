7f8eea472907ba18608509df97eefd8b114ed54c
komalsatale <123380647+komalsatale@users.noreply.github.com>
2024-09-28 22:03:15 +0530

    added Traditional css

---
+++ src/Components/styling Css/TraditionalCss.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function TraditionalCss(props) {
+    return (
+        <div>
+          <h3>Traditional Css</h3>  
+        </div>
+    );
+}
+
+export default TraditionalCss;
